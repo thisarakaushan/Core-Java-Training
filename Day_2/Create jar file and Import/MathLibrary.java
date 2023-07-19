74c31995bc748b792e47378274ae919acc70618e
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-07-19 21:49:09 +0530

    Add files via upload

---
+++ Day_2/Create jar file and Import/MathLibrary.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+public class MathLibrary {
+	
+	public static int add(int a, int b) {
+		
+		return a+b;
+	}
+	public static int sub(int a, int b) {
+		
+		return a-b;
+	}
+	public static int mul(int a, int b) {
+	
+	return a*b;
+	}
+	public static int div(int a, int b) {
+	
+	return a/b;
+	}
+}
---
+++ Day_2/Create jar file and Import/MyClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println(MathLibrary.add(4,5));
+		System.out.println(MathLibrary.sub(4,5));
+		System.out.println(MathLibrary.mul(4,5));
+		System.out.println(MathLibrary.div(4,5));
+	}
+
+}
