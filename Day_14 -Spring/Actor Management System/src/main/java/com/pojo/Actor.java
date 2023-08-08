be81269664dee0a2ad4dea1fb022bcf20c674092
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 19:49:26 +0530

    Add files via upload

--- Day_14 -Spring/Actor Management System/src/main/java/com/pojo/Actor.java
+++ Day_14 -Spring/Actor Management System/src/main/java/com/pojo/Actor.java
@@ -1,1 +1,36 @@
+package com.highradius.pojo;
 
+public class Actor {
+    private int actorId;
+    private String firstName;
+    private String lastName;
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor [actorId=" + actorId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+}
