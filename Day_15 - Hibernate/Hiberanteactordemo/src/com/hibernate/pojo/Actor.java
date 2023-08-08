2a11245d9e7fd98676b722a12bc83b786d06c521
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 20:32:45 +0530

    Add files via upload

--- Day_15 - Hibernate/Hiberanteactordemo/src/com/hibernate/pojo/Actor.java
+++ Day_15 - Hibernate/Hiberanteactordemo/src/com/hibernate/pojo/Actor.java
@@ -1,1 +1,33 @@
+package com.highradius.hiberante.pojo;
 
+public class Actor {
+	
+	public Actor() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	
+
+}
