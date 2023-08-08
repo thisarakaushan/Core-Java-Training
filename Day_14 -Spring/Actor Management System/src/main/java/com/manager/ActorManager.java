4f0f49ad107150970259f438c645a3a0e1a758a0
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 19:48:27 +0530

    Add files via upload

--- Day_14 -Spring/Actor Management System/src/main/java/com/manager/ActorManager.java
+++ Day_14 -Spring/Actor Management System/src/main/java/com/manager/ActorManager.java
@@ -1,1 +1,15 @@
+package com.highradius.manager;
 
+import java.util.List;
+import com.highradius.pojo.Actor;
+
+public interface ActorManager {
+	
+    List<Actor> getAllActors();
+    
+    int addActor(Actor actor);
+
+    int updateActor(Actor actor);
+
+	int deleteActor(Actor actor);
+}
