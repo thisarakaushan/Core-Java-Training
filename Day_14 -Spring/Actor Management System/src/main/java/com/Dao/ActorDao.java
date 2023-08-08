0946e8d26ca396d35f0c4d1aee9504cad565827d
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 19:47:46 +0530

    Add files via upload

--- Day_14 -Spring/Actor Management System/src/main/java/com/Dao/ActorDao.java
+++ Day_14 -Spring/Actor Management System/src/main/java/com/Dao/ActorDao.java
@@ -1,1 +1,17 @@
+package com.highradius.Dao;
 
+import java.util.List;
+import com.highradius.pojo.Actor;
+
+public interface ActorDao {
+	
+    List<Actor> getAllActors();
+
+    int addActor(Actor actor);
+
+    int updateActor(Actor actor);
+
+    int deleteActor(Actor actor);
+
+	boolean exist(Actor actor);
+}
