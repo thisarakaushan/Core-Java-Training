f51d378f00e078db8b7f4f174e855ab87598cfac
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 19:48:46 +0530

    Add files via upload

--- Day_14 -Spring/Actor Management System/src/main/java/com/managerImpl/ActorManagerImpl.java
+++ Day_14 -Spring/Actor Management System/src/main/java/com/managerImpl/ActorManagerImpl.java
@@ -1,1 +1,46 @@
+ package com.highradius.managerImpl;
 
+import java.util.List;
+
+import com.highradius.Dao.ActorDao;
+import com.highradius.DaoImpl.ActorDaoImpl;
+import com.highradius.pojo.Actor;
+import com.highradius.manager.ActorManager;
+
+public class ActorManagerImpl implements ActorManager {
+	
+	ActorDao actorDao = new ActorDaoImpl();
+
+    public ActorDao getActorDao() {
+        return actorDao;
+    }
+
+    public void setActorDao(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
+    @Override
+    public List<Actor> getAllActors() {
+        return actorDao.getAllActors();
+    }
+
+    @Override
+    public int addActor(Actor actor) {
+    	if(!actorDao.exist(actor)) {
+    		
+    		actorDao.addActor(actor);
+    	}
+        return -1; // Indicates that the actor already exists
+    }
+
+    @Override
+    public int updateActor(Actor actor) {
+        return actorDao.updateActor(actor);
+    }
+
+    @Override
+    public int deleteActor(Actor actor) {
+       return actorDao.deleteActor(actor);
+    }
+    
+}
