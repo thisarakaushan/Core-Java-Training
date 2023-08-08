21ade4f402ae457e65888daf9d765d197232e29b
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-08 20:29:11 +0530

    Add files via upload

--- Day_15 - Hibernate/Hibernatedemo/src/com/hibernate/pojo/Customer.java
+++ Day_15 - Hibernate/Hibernatedemo/src/com/hibernate/pojo/Customer.java
@@ -1,1 +1,25 @@
+package com.highradius.hiberante.pojo;
 
+public class Customer {
+	
+	private int id;
+	private String firstName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public Customer() {
+
+		// TODO Auto-generated constructor stub
+	}
+
+}
