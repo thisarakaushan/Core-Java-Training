f852e7cd51ebaf4a4817d1d1d3e859efd7fdffe2
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-09-18 17:10:57 +0530

    Add files via upload

--- Product Platform/Day 8/MultiThreading/mypackage/thread/ThreadDemo1.java
+++ Product Platform/Day 8/MultiThreading/mypackage/thread/ThreadDemo1.java
@@ -1,1 +1,46 @@
+package mypackage.thread;
 
+class Hi {
+	
+	public void show() {
+		for(int i = 0; i<=5; i++) {
+			System.out.println("Hi");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Hello {
+	
+	public void show() {
+		
+		for(int i = 0; i<=5; i++) {
+			System.out.println("Hello");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
+
+public class ThreadDemo1 {
+	
+	public static void main(String[] args) {
+		
+		Hi hi = new Hi();
+		Hello hello = new Hello();
+		
+		hi.show();
+		hello.show();
+		
+	}
+}
